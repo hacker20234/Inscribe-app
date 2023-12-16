13556c707bed1d12beadc19793ab506426817818
xgocn <34662366@qq.com>
2023-12-17 01:50:29 +0800

    init

--- chakra-ui-inscribe-dapp/src/pages/index.tsx
+++ chakra-ui-inscribe-dapp/src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './home';
 import { Main } from './main';
+import { AllNFTCollection } from './nfts';
 
-const routes = [{ path: '/', Page: Home },{ path: '/main', Page: Main }];
+const routes = [
+  { path: '/', Page: Home },
+  { path: '/main', Page: Main },
+  { path: '/nfts', Page: AllNFTCollection }
+];
 
 function Routing() {
   const getRoutes = () => routes.map(({ path, Page }) => <Route key={path} path={path} element={<Page />} />);
---
+++ chakra-ui-inscribe-dapp/src/pages/nfts/index.ts
@@ -0,0 +1,3 @@
+import { AllNFTCollection } from './Nfts';
+
+export { AllNFTCollection };
