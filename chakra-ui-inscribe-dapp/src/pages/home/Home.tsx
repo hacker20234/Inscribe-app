fa267116460266ae5ab412d6e743e09a07b8bb33
123 <>
2023-12-19 17:48:20 +0700

    fix

--- chakra-ui-inscribe-dapp/src/pages/home/Home.tsx
+++ chakra-ui-inscribe-dapp/src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
-import { Box, Text, Input } from "@chakra-ui/react";
 import { useRef, useEffect } from "react";
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from "@chakra-ui/react";
+import "./style.css";
 
 function Home() {
   const inputRef = useRef<HTMLInputElement | null>(null);
@@ -11,10 +12,189 @@ function Home() {
   }, []);
 
   return (
-    <Box>
-      <Text color="red">Hola Mundo</Text>
-      <Input ref={inputRef} border="1px solid black" width="200px" focusBorderColor="blue.500" />
-    </Box>
+    <TableContainer>
+      <Table variant="simple">
+        {/* 表格底部描述 */}
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        {/* 表头字段 */}
+        <Thead>
+          <Tr>
+            <Th>inscribeType</Th>
+            <Th>inscribeIndex</Th>
+            <Th>deployer</Th>
+            <Th>tick</Th>
+            <Th>maxSupply</Th>
+            <Th>totalSupply</Th>
+            <Th isNumeric>amtPerMint</Th>
+            <Th>slogan</Th>
+            <Th>media</Th>
+            <Th>mediaLink</Th>
+            <Th>verify</Th>
+            <Th>icon</Th>
+            <Th>frame</Th>
+            <Th>decimals</Th>
+            <Th>inscribeState</Th>
+          </Tr>
+        </Thead>
+
+        {/* 表格主体数据 */}
+        <Tbody>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+          <Tr>
+            <Td>Organization</Td>
+            <Td>1</Td>
+            <Td>0x84e7195c2a5b52620190f9303897e74676be2781e308fd8176c87037e9402065</Td>
+            <Td>VEIN</Td>
+            <Td>1,000,000,000,000</Td>
+            <Td>0</Td>
+            <Td isNumeric>1,000</Td>
+            <Td>WE DO THE BEST</Td>
+            <Td>Twitter</Td>
+            <Td>https://x.com/vein</Td>
+            <Td>None</Td>
+            <Td>https://ipfs.io/icon</Td>
+            <Td>https://ipfs.io/frame</Td>
+            <Td>0</Td>
+            <Td>MintStart</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 }
 
