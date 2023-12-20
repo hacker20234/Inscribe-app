408bfb4ae3651927c75713533825d887fdade9a7
123 <>
2023-12-20 17:40:48 +0700

    fix

--- chakra-ui-inscribe-dapp/src/hocs/index.tsx
+++ chakra-ui-inscribe-dapp/src/hocs/index.tsx
@@ -1,13 +1,8 @@
-import {
-  ApiProvider as GearApiProvider,
-  AlertProvider as GearAlertProvider,
-  AccountProvider,
-  ProviderProps,
-} from '@gear-js/react-hooks';
-import { Alert, alertStyles } from '@gear-js/ui';
-import { ComponentType } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { ADDRESS } from 'consts';
+import { ApiProvider as GearApiProvider, AlertProvider as GearAlertProvider, AccountProvider, ProviderProps } from "@gear-js/react-hooks";
+import { Alert, alertStyles } from "@gear-js/ui";
+import { ComponentType } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ADDRESS } from "consts";
 
 function ApiProvider({ children }: ProviderProps) {
   return <GearApiProvider providerAddress={ADDRESS.NODE}>{children}</GearApiProvider>;
